656763103b36c89c3215ad5008156ea2db342bfa
Bert Chun Chang <b.c.chang@outlook.com>
2026-08-14 18:55:58 +0800

    [fix] add necessary boundary check

--- exercises/04-two_ptrs_&_sliding_window/02-max_sum_of_ctgs_subarr.cpp
+++ exercises/04-two_ptrs_&_sliding_window/02-max_sum_of_ctgs_subarr.cpp
@@ -6,7 +6,7 @@ using std::vector;
 // Recognition trigger: fixed-size sliding window
 
 int maxCtgsSubarraySum(const vector<int>& nums, int k) {
-    int left = 0;
+    if ((int)nums.size() < k) return -1;
     int max_sum = std::accumulate(nums.begin(), nums.begin() + k, 0);
     int curr_sum = max_sum;
     for (int right = k; right < (int)nums.size(); right++) {
@@ -20,13 +20,10 @@ int maxCtgsSubarraySum(const vector<int>& nums, int k) {
 int main() {
     vector<int> arr1 = {2, 1, 5, 1, 3, 2};
     int k1 = 3;
-
     vector<int> arr2 = {2, 3, 4, 1, 5};
     int k2 = 2;
-
     vector<int> arr3 = {1};
     int k3 = 1;
-
     std::cout << maxCtgsSubarraySum(arr1, k1) << '\n';
     std::cout << maxCtgsSubarraySum(arr2, k2) << '\n';
     std::cout << maxCtgsSubarraySum(arr3, k3) << '\n';
