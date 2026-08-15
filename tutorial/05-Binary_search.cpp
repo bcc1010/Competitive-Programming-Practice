e6d23a9bade69ae5f3f7d83f3d1505ef5817ee8e
Bert Chun Chang <b.c.chang@outlook.com>
2026-08-15 16:33:11 +0800

    [feat] add left/rightmost occurrence

--- tutorial/05-Binary_search.cpp
+++ tutorial/05-Binary_search.cpp
@@ -18,3 +18,25 @@ int binarySearch(const vector<int>& arr, int target) {
     }
     return -1;
 }
+
+int leftMostOccurrence(const vector<int>& arr, int target) {
+    int left = 0;
+    int right = (int)arr.size() - 1;
+    int result = -1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target) {
+            result = mid;
+            right = mid - 1;        // Keep searching LEFT!!
+            // right = mid + 1;     // or, alt, keep searching right!!
+        } else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return result;
+}
+
