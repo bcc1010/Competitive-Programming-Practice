97cc7ebb630ffd4b784ad82d1bfb1d1bf8d7d66d
Bert Chun Chang <b.c.chang@outlook.com>
2026-08-21 21:32:32 +0800

    [feat] add largest histogram rectangle prac soln

---
+++ tutorial/06-stacks_queues_&_monotonic_structs/largest_histogram_rectangle.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <stack>
+using std::vector;
+using std::stack;
+
+int largestHistogramRectangle(const vector<int> heights) {
+    stack<int> stk;
+    int best = 0;
+    int n = (int)heights.size();
+
+    for (int i = 0; i <= n; i++) {
+        // Use height 0 as a sentinel value at the end to force popping
+        int currHeight = (i == n) ? 0 : heights[i];
+        
+        while (!stk.empty() && heights[stk.top()] > currHeight) {
+            int height = heights[stk.pop()];
+            stk.pop();
+
+            // Width: from the new stack top to current position
+            int width = stk.empty() ? i : i - stk.top() - 1;
+            best = std::max(best, height * width);
+        }
+        stk.push(i);
+    }
+    return best;
+}
